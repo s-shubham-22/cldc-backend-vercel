f829d3ef10a160979209c35e95e8770e4003a0dc
Shubham Sareliya <sareliashubham9999@gmail.com>
2023-07-17 10:38:57 +0530

    vercel updated

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.listen(PORT, async () => {
   console.log(`🚀 Server running on port: ${PORT}`);
   try {
     await connectDB();
-    sequelize.sync({ force: false })
+    sequelize.sync({ alter: true })
       .then(async () => {
         console.log('✅ Database & tables synced!');
         // await seed();
